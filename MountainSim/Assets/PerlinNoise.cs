6511157781dc1161cd12141b2994ac9f600fa3e5
DanyDiab <danydiab04@gmail.com>
2025-07-20 14:04:38 -0300

    added point cell determination

--- MountainSim/Assets/PerlinNoise.cs
+++ MountainSim/Assets/PerlinNoise.cs
@@ -16,13 +16,14 @@ public class PerlinNoise : MonoBehaviour
         cellSize = 4;
         gridSize = 100;    
         gradientVectors = new Vector3[gridSize, gridSize];
+        generatePerlinNoise();
+
+    }
+    void generatePerlinNoise(){
         generateGraidentVectors();
-
+        determineCells();
     }
 
-    void Update()
-    {
-    }
 
     void generateGraidentVectors(){
         for(int i = 0; i < gridSize; i++){
@@ -31,10 +32,39 @@ public class PerlinNoise : MonoBehaviour
                 float randDirX = Random.Range(-1f,1f);
                 float randDirY = Random.Range(-1f,1f);
                 Vector3 gradientVector = new Vector3(randDirX,randDirY, 0).normalized;
-                Debug.Log(gradientVector);
                 gradientVectors[i,j] = gradientVector;
                 Debug.DrawLine(origin, origin + gradientVector * cellSize, Color.green, 100f);
             }   
         }
     }
+
+    void determineCells(){
+        for(int i = 0; i < gridSize; i++){
+            for(int j = 0; j < gridSize; j++){
+                Vector3 point = new Vector3(i,j,0);
+                Vector2 cell = determineCell(point);
+                Debug.Log(cell);
+            }
+        }
+    }
+
+
+
+
+    Vector3 determineCell(Vector3 point){
+        int cellX = (int)point.x / cellSize;
+        int cellY = (int)point.y / cellSize;
+        Vector3 cell = new Vector3(cellX,cellY,0);
+        return cell;
+    }
 }
+
+
+// steps
+// generate X vectors, point them in random directions
+// for each point in the grid:
+    // determine which cell it belongs in
+    // calculate influence of each of the 4 corners to the point
+    // compute dot product of offset and gradient vectors
+    // fade the fractinals 
+    // interpolate the dot products using the faded vault

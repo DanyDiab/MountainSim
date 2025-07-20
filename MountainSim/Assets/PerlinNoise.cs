f4cccb5f686b5803d02707221dc4c1ba55b57b6a
DanyDiab <danydiab04@gmail.com>
2025-07-20 15:46:06 -0300

    perlin noise fully generated, needs to be displayed

--- MountainSim/Assets/PerlinNoise.cs
+++ MountainSim/Assets/PerlinNoise.cs
@@ -1,27 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PerlinNoise : MonoBehaviour
 {
     int cellSize;
     // x by x grid of X cells
     int gridSize; 
-    
+
     Vector3[,] gradientVectors;
 
 
     void Start()
     {
         cellSize = 4;
-        gridSize = 100;    
+        gridSize = 100;
         gradientVectors = new Vector3[gridSize, gridSize];
         generatePerlinNoise();
 
     }
     void generatePerlinNoise(){
         generateGraidentVectors();
-        determineCells();
+        getPerlinValues();
     }
 
 
@@ -33,30 +32,79 @@ public class PerlinNoise : MonoBehaviour
                 float randDirY = Random.Range(-1f,1f);
                 Vector3 gradientVector = new Vector3(randDirX,randDirY, 0).normalized;
                 gradientVectors[i,j] = gradientVector;
-                Debug.DrawLine(origin, origin + gradientVector * cellSize, Color.green, 100f);
             }   
         }
     }
-
-    void determineCells(){
-        for(int i = 0; i < gridSize; i++){
-            for(int j = 0; j < gridSize; j++){
-                Vector3 point = new Vector3(i,j,0);
-                Vector2 cell = determineCell(point);
-                Debug.Log(cell);
-            }
-        }
-    }
-
-
-
-
     Vector3 determineCell(Vector3 point){
         int cellX = (int)point.x / cellSize;
         int cellY = (int)point.y / cellSize;
         Vector3 cell = new Vector3(cellX,cellY,0);
         return cell;
     }
+
+    (Vector3,Vector3,Vector3,Vector3,Vector3,Vector3,Vector3,Vector3)calculateOffsetVectors(Vector3 point, Vector3 cell){
+        int posX = (int) cell.x;
+        int posY = (int) cell.y;
+        Vector3 blCell = cell;
+        Vector3 brCell = new Vector3(posX + cellSize, posY);
+        Vector3 tlCell = new Vector3(posX, posY + cellSize);
+        Vector3 trCell = new Vector3(posX + cellSize, posY + cellSize);
+
+        Vector3 bl = point - blCell;
+        Vector3 br = point - brCell;
+        Vector3 tl = point - tlCell;
+        Vector3 tr = point - trCell;
+        return (tl,tr,bl,br, tlCell, trCell, blCell, brCell);
+    }
+
+    void getPerlinValues(){
+        for(int i = 0; i < gridSize; i++){
+            for(int j = 0; j < gridSize; j++){
+                Vector3 point = new Vector3(i,j,0);
+                Vector3 cell = determineCell(point);
+                (
+                    Vector3 tl, Vector3 tr, Vector3 bl, Vector3 br, 
+                    Vector3 tlCell, Vector3 trCell, Vector3 blCell, Vector3 brCell
+                ) = calculateOffsetVectors(point, cell);
+
+                float tlI = Vector3Dot(tl ,tlCell);
+                float trI = Vector3Dot(tr, trCell);
+                float blI = Vector3Dot(bl, blCell);
+                float brI = Vector3Dot(br, brCell);
+
+                float localX = (point.x - cell.x) / cellSize;
+                float localY = (point.y - cell.y) / cellSize;
+
+                float u = fade(localX);
+                float v = fade(localY);
+
+                float lerpBottom = lerp(blI, brI, u);
+                float lerpTop = lerp(tlI,trI, u);
+
+                float finalLerp = lerp(lerpBottom,lerpTop, v);
+
+                // draw noise call goes here
+            }
+        }
+    }
+
+    float fade(float t){
+        return t * t * t * (t * (t * 6 - 15) + 10);
+    }
+
+    float lerp(float a, float b, float t){
+        return a + (b - a) * t;
+    }
+    
+
+
+    float Vector3Dot(Vector3 v1, Vector3 v2){
+        return (v1.x * v2.x) + (v1.y * v2.y) + (v1.z * v2.z);
+    }
+
+
+    
+
 }
 
 

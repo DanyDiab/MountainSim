7f74841a845c2655e20c52df81ff54dffc3f65ff
DanyDiab <danydiab04@gmail.com>
2025-07-25 19:01:45 -0300

    perlin noise fully functional now!

--- MountainSim/Assets/PerlinNoise.cs
+++ MountainSim/Assets/PerlinNoise.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SocialPlatforms;
 
 public class PerlinNoise : MonoBehaviour
 {
@@ -6,16 +7,16 @@ public class PerlinNoise : MonoBehaviour
     // x by x grid of X cells
     int gridSize; 
     Texture2D noiseTexture;
-    Vector3[,] gradientVectors;
+    Vector2[,] gradientVectors;
     public Renderer targetRenderer;
 
 
     void Start()
     {
         cellSize = 10;
-        gridSize = 200;
-        gradientVectors = new Vector3[gridSize + 1, gridSize + 1];
-        noiseTexture = new Texture2D(gridSize,gridSize);
+        gridSize = 20;
+        gradientVectors = new Vector2[gridSize + 1, gridSize + 1];
+        noiseTexture = new Texture2D(gridSize * cellSize,gridSize * cellSize);
 
         generatePerlinNoise();
     }
@@ -51,30 +52,39 @@ public class PerlinNoise : MonoBehaviour
         Color[] pixels = new Color[gridSize * gridSize];
         float maxObserved = float.MinValue;
         float minObserved = float.MaxValue;
-        for(int i = 0; i < gridSize; i++){
-            for(int j = 0; j < gridSize; j++){
-                Vector2 point = new Vector2(i,j);
+        for(int i = 0; i < noiseTexture.height; i++){
+            for(int j = 0; j < noiseTexture.width; j++){
                 // calculate percent of cell which contains the point
-                float pointNormalX = point.x % cellSize / cellSize;
-                float pointNormalY = point.y % cellSize / cellSize;
+                float sampleX = (float)j / cellSize;
+                float sampleY = (float)i / cellSize;
+                int gridX = Mathf.FloorToInt(sampleX);
+                int gridY = Mathf.FloorToInt(sampleY);
+
+
+                float LocalX = sampleX - gridX;
+                float LocalY = sampleY - gridY;
+
+                Debug.Log(LocalX);
+                Debug.Log(LocalY);
+
                 // calculate the apropraite grid intersections
-                Vector2 tl = new Vector2(point.x % (gridSize /cellSize) ,point.y % (gridSize /cellSize));
-                Vector2 tr = new Vector2((point.x % (gridSize /cellSize)) + 1 ,point.y % (gridSize /cellSize));
-                Vector2 bl = new Vector2(point.x % (gridSize /cellSize) ,(point.y % (gridSize /cellSize)) + 1);
-                Vector2 br = new Vector2((point.x % (gridSize /cellSize)) + 1 ,(point.y % (gridSize /cellSize)) + 1);
+                Vector2 tl = new Vector2(LocalX, LocalY);
+                Vector2 tr = new Vector2(LocalX - 1 ,LocalY);
+                Vector2 bl = new Vector2(LocalX ,LocalY - 1);
+                Vector2 br = new Vector2(LocalX - 1 ,LocalY - 1);
                 // find the apropriate gradientVectors
-                Vector2 tlGrad = gradientVectors[(int)tl.x,(int)tl.y];
-                Vector2 trGrad = gradientVectors[(int)tr.x,(int)tr.y];
-                Vector2 blGrad = gradientVectors[(int)bl.x,(int)bl.y];
-                Vector2 brGrad = gradientVectors[(int)br.x,(int)br.y];
+                Vector2 tlGrad = gradientVectors[gridX,gridY];
+                Vector2 trGrad = gradientVectors[gridX + 1,gridY];
+                Vector2 blGrad = gradientVectors[gridX,gridY + 1];
+                Vector2 brGrad = gradientVectors[gridX + 1, gridY + 1];
                 // calculate the influences of gradient vectors on the point
                 float tlI = Vector2Dot(tl,tlGrad);
                 float trI = Vector2Dot(tr,trGrad);
                 float blI = Vector2Dot(bl,blGrad);
                 float brI = Vector2Dot(br,brGrad);
                 // fade to smooth the values
-                float u = fade(pointNormalX);
-                float v = fade(pointNormalY);
+                float u = fade(LocalX);
+                float v = fade(LocalY);
                 // lerp across the top and bottom
                 float top = lerp(tlI,trI,u);
                 float bot = lerp(blI,brI,u);
@@ -86,8 +96,9 @@ public class PerlinNoise : MonoBehaviour
                 if(final < minObserved){
                     minObserved = final;
                 }
+                float brightness = valueToBrightness(final);
                 // noiseTexture.SetPixel
-                noiseTexture.SetPixel(i,j, new Color(final,final,final));
+                noiseTexture.SetPixel(j,i, new Color(brightness,brightness,brightness));
 
             }
         }
@@ -104,6 +115,9 @@ public class PerlinNoise : MonoBehaviour
             Debug.LogWarning("Target Renderer not assigned. Please assign a Plane or Quad to display the noise.");
         }
     }
+    float valueToBrightness(float value){
+        return (value + 1f) / 2f;
+    }
 
     float fade(float t){
         return t * t * t * (t * (t * 6 - 15) + 10);
@@ -113,10 +127,6 @@ public class PerlinNoise : MonoBehaviour
         return a + (b - a) * t;
     }
     
-    float valueToBrightness(float value){
-        float brightness = (value + 1) / 2;
-        return brightness;
-    }
 
     float Vector2Dot(Vector2 v1, Vector2 v2){
         return (v1.x * v2.x) + (v1.y * v2.y);

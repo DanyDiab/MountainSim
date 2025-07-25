836b0d7545ed2a9ed927ef84792c57b567562bcb
DanyDiab <danydiab04@gmail.com>
2025-07-24 21:53:18 -0300

    reimplemented

--- MountainSim/Assets/PerlinNoise.cs
+++ MountainSim/Assets/PerlinNoise.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityEngine.UI;
 
 public class PerlinNoise : MonoBehaviour
 {
@@ -15,8 +14,8 @@ public class PerlinNoise : MonoBehaviour
 
     void Start()
     {
-        cellSize = 2;
-        gridSize = 100;
+        cellSize = 5;
+        gridSize = 20;
         gradientVectors = new Vector3[gridSize + 1, gridSize + 1];
         generatePerlinNoise();
         renderTexture();
@@ -31,73 +30,61 @@ public class PerlinNoise : MonoBehaviour
     void generateGraidentVectors(){
         for(int i = 0; i < gridSize + 1; i++){
             for(int j = 0; j < gridSize + 1; j++){
-                Vector3 origin = new Vector3(i * cellSize, j * cellSize, 0);
+                Vector2 origin = new Vector2(i * cellSize, j * cellSize);
                 float randDirX = Random.Range(-1f,1f);
                 float randDirY = Random.Range(-1f,1f);
-                Vector3 gradientVector = new Vector3(randDirX,randDirY, 0).normalized;
+                Vector2 gradientVector = new Vector2(randDirX,randDirY).normalized;
                 gradientVectors[i,j] = gradientVector;
             }   
         }
     }
     Vector3 determineCell(Vector3 point){
-        int cellX = (int)point.x / cellSize;
-        int cellY = (int)point.y / cellSize;
+        int cellX = (int)Mathf.Floor(point.x / cellSize);
+        int cellY = (int)Mathf.Floor(point.y / cellSize);
         Vector3 cell = new Vector3(cellX,cellY,0);
         return cell;
     }
 
 // returning the first 4 vectors (off set vectors)
 // returning the last 4 vectors (the corresponding cells)
-    (Vector3,Vector3,Vector3,Vector3)calculateOffsetVectors(Vector3 point, Vector3 cell){
-        int posX = (int) cell.x;
-        int posY = (int) cell.y;
-        Vector3 blCell = cell;
-        Vector3 brCell = new Vector3(posX + cellSize, posY);
-        Vector3 tlCell = new Vector3(posX, posY + cellSize);
-        Vector3 trCell = new Vector3(posX + cellSize, posY + cellSize);
-
-        Vector3 bl = point - blCell;
-        Vector3 br = point - brCell;
-        Vector3 tl = point - tlCell;
-        Vector3 tr = point - trCell;
-        return (tl,tr,bl,br);
-    }
 
     void getPerlinValues(){
         Color[] pixels = new Color[gridSize * gridSize];
         Texture2D noiseTexture = new Texture2D(gridSize,gridSize);
+        float maxObserved = 0;
         for(int i = 0; i < gridSize; i++){
             for(int j = 0; j < gridSize; j++){
-                Vector3 point = new Vector3(i,j,0);
-                Vector3 cell = determineCell(point);
-                (Vector3 tl, Vector3 tr, Vector3 bl, Vector3 br) = calculateOffsetVectors(point, cell);
-                float tlI = Vector3Dot(tl , gradientVectors[(int)cell.x,(int)cell.y]);
-                float trI = Vector3Dot(tr, gradientVectors[(int)cell.x + 1,(int)cell.y]);
-                float blI = Vector3Dot(bl, gradientVectors[(int)cell.x,(int)cell.y + 1]);
-                float brI = Vector3Dot(br, gradientVectors[(int)cell.x + 1,(int)cell.y + 1]);
-                
-                // Debug.Log(tlI);
-                // Debug.Log(trI);
-                // Debug.Log(blI);
-                // Debug.Log(brI);
-                // Vector3 pointLocalized = point / cellSize; 
-                float localX = (point.x - cell.x) / cellSize;
-                float localY = (point.y - cell.y) / cellSize;
-                Debug.Log("sub value" + (point - cell));
-                Debug.Log("point" + point);
-                Debug.Log("cell" + cell);
-                // Debug.Log("localX" + localX);
-                // Debug.Log("localY" + localY);
-                float u = fade(localX);
-                float v = fade(localY);
+                Vector2 point = new Vector2(i,j);
+                // calculate percent of cell which contains the point
+                float pointNormalX = point.x % cellSize / cellSize;
+                float pointNormalY = point.y % cellSize / cellSize;
+                // calculate the apropraite grid intersections
+                Vector2 tl = new Vector2(point.x % (gridSize /cellSize) ,point.y % (gridSize /cellSize));
+                Vector2 tr = new Vector2((point.x % (gridSize /cellSize)) + 1 ,point.y % (gridSize /cellSize));
+                Vector2 bl = new Vector2(point.x % (gridSize /cellSize) ,(point.y % (gridSize /cellSize)) + 1);
+                Vector2 br = new Vector2((point.x % (gridSize /cellSize)) + 1 ,(point.y % (gridSize /cellSize)) + 1);
+                // find the apropriate gradientVectors
+                Vector2 tlGrad = gradientVectors[(int)tl.x,(int)tl.y];
+                Vector2 trGrad = gradientVectors[(int)tr.x,(int)tr.y];
+                Vector2 blGrad = gradientVectors[(int)bl.x,(int)bl.y];
+                Vector2 brGrad = gradientVectors[(int)br.x,(int)br.y];
+                // calculate the influences of gradient vectors on the point
+                float tlI = Vector2Dot(tl,tlGrad);
+                float trI = Vector2Dot(tr,trGrad);
+                float blI = Vector2Dot(bl,blGrad);
+                float brI = Vector2Dot(br,brGrad);
+                // fade to smooth the values
+                float u = fade(pointNormalX);
+                float v = fade(pointNormalY);
+                // lerp across the top and bottom
+                float top = lerp(tlI,trI,u);
+                float bot = lerp(blI,brI,u);
+                // lerp the top and bottom
+                float final = lerp(top,bot,v);
+                pixels[j * gridSize + i] = new Color(final,final,final);
+                Debug.Log(final);
+                // maxObserved
 
-                float lerpBottom = lerp(blI, brI, u);
-                float lerpTop = lerp(tlI,trI, u);
-
-                float finalLerp = lerp(lerpBottom,lerpTop, v);
-                // Debug.Log(finalLerp);
-                pixels[j * gridSize + i] = new Color(finalLerp,finalLerp,finalLerp);
-                // draw noise call goes here
             }
         }
         noiseTexture.SetPixels(pixels);
@@ -126,8 +113,8 @@ public class PerlinNoise : MonoBehaviour
         return brightness;
     }
 
-    float Vector3Dot(Vector3 v1, Vector3 v2){
-        return (v1.x * v2.x) + (v1.y * v2.y) + (v1.z * v2.z);
+    float Vector2Dot(Vector2 v1, Vector2 v2){
+        return (v1.x * v2.x) + (v1.y * v2.y);
     }
 
 }

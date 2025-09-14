3e0f0904615fcd0fa85556cda6fe1f421a90052b
DanyDiab <danydiab04@gmail.com>
2025-09-14 17:13:15 -0300

    added a fix to prevent top bound being allocated too many space due to an off by one error

--- MountainSim/Assets/Scripts/TerrainColoring.cs
+++ MountainSim/Assets/Scripts/TerrainColoring.cs
@@ -26,7 +26,7 @@ public class TerrainColoring : MonoBehaviour
     
     public void updatePixelColors(){
         mesh = meshFilter.mesh;
-        determineBounds(mesh);
+        determineBounds(mesh, colors.Length);
         verticies = mesh.vertices;
         pixelColors = new Color[verticies.Length];
         for(int i = 0; i < verticies.Length; i++){
@@ -60,14 +60,13 @@ public class TerrainColoring : MonoBehaviour
 
     public void updatePixelTex(){
         mesh = meshFilter.mesh;
-        determineBounds(mesh);
+        determineBounds(mesh, textures.Length);
         shaderMat.SetFloatArray("_Heights", bounds);
         shaderMat.SetInt("_numBounds", bounds.Length);
         Texture2DArray texArray = new Texture2DArray(
             1024, 1024, bounds.Length, TextureFormat.DXT1, true
         );
         for (int i = 0; i < textures.Length; i++) {
-            Debug.Log(textures[i].format);
             Graphics.CopyTexture(textures[i], 0, 0, texArray, i, 0);
         }
         shaderMat.SetTexture("_Textures", texArray);   
@@ -97,17 +96,14 @@ public class TerrainColoring : MonoBehaviour
 
     }
 
-    void determineBounds(Mesh mesh){
-        bounds = new float[colors.Length];
+    void determineBounds(Mesh mesh, int len){
+        bounds = new float[len];
         float max = mesh.bounds.max.y;
         float min = mesh.bounds.min.y;
-        Debug.Log(max);
-        Debug.Log(min);
-
         float heightDiff = max - min;
-        float stepSize = heightDiff / colors.Length;
+        float stepSize = heightDiff / (len - 1);
         float currPos = min;
-        for(int i = 0; i < colors.Length; i++){
+        for(int i = 0; i < len; i++){
             bounds[i] = currPos;
             currPos += stepSize;
         }

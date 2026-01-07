50542e313911ac53c454bccdf463aee7ccf034c7
DanyLaptop <DanyDiab04@gmail.com>
2026-01-07 16:32:16 -0400

    Refactor: Move Nature and Engineering assets to Imported_Assets

---
+++ MountainSim/Assets/Scripts/Util/SkyboxSwapper.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SkyboxSwapper : MonoBehaviour {
+    [Header("UI Reference")]
+    [SerializeField] TMP_Dropdown dropdown;
+
+    [Header("Data")]
+    [SerializeField] List<Material> skyboxMaterials;
+
+    void Start() {
+        if (dropdown.options.Count != skyboxMaterials.Count) {
+            Debug.LogWarning("Warning: Dropdown options count does not match Skybox Materials count.");
+        }
+
+        dropdown.onValueChanged.AddListener(SwapToSelected);
+    }
+
+    void SwapToSelected(int index) {
+        if (index < 0 || index >= skyboxMaterials.Count) {
+            return;
+        }
+
+        RenderSettings.skybox = skyboxMaterials[index];
+
+        // DynamicGI.UpdateEnvironment();
+    }
+}
--- notes.md
+++ notes.md
@@ -3,14 +3,13 @@
 
 ## todo 
 
-- Make Cursor invisible by default, shows when menu is open
-
-
 - Add Controls Menu
 - Main Menu
 - Add a bunch more textures, more variety
 - Toast message to display alerts
 - loading symbol to show terrain is generating
+- explore skybox
+
 
 ## optimizations
 
